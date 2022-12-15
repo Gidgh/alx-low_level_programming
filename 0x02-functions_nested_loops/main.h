c5b230f6ea0c055924f9d7c231325321197c94b4
Gid <gideonandrewedu@gmail.com>
2022-12-15 09:23:10 -0800

    print to 98

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +11,3 @@ int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 void print_times_table(int n);
-
-#endif
